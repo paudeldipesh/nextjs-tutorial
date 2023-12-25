879a2c7a2e39c92abbe6c37bbe4ca404170ba14b
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-25 09:29:49 +0545

    add pages and navbar

---
+++ app/prisma-example/page.js
@@ -0,0 +1,3 @@
+export default function PrismaExamplePage() {
+  return <div>PrismaExamplePage</div>;
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+const links = [
+  {
+    href: "/client",
+    label: "client",
+  },
+  {
+    href: "/drinks",
+    label: "drinks",
+  },
+  {
+    href: "/tasks",
+    label: "tasks",
+  },
+  {
+    href: "/query",
+    label: "query",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <nav className="bg-base-300 py-4">
+      <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
+        <Link href="/" className="btn btn-primary">
+          Next.js
+        </Link>
+        <ul className="menu menu-horizontal md:ml-8">
+          {links.map(({ href, label }) => (
+            <li key={href} className="capitalize">
+              <Link href={href}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
