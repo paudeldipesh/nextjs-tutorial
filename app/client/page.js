43a590ec1bced9d83291771cab0edccee0d3ebda
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-25 10:14:01 +0545

    setup client page

--- app/client/page.js
+++ app/client/page.js
@@ -1,3 +1,18 @@
+"use client";
+import { useState } from "react";
+
 export default function ClientPage() {
-  return <div>ClientPage</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-7xl mb-4 font-bold">{count}</h1>
+      <button
+        onClick={() => setCount((cur) => cur + 1)}
+        className="btn btn-primary capitalize"
+      >
+        increase
+      </button>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
+      <Link href="/client" className="btn btn-accent capitalize">
+        get started
+      </Link>
+    </div>
+  );
 }
