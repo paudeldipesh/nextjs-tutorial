4f75406c9a19f73e22adff648f2f9aaf93d5b2c2
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-27 10:32:39 +0545

    setup prisma

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,3 +1,13 @@
+import prisma from "@/utils/db";
+
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: "wake up",
+    },
+  });
+};
+
 export default function PrismaExamplePage() {
   return <div>PrismaExamplePage</div>;
 }
--- app/query/page.js
+++
@@ -1,3 +0,0 @@
-export default function QueryPage() {
-  return <div>QueryPage</div>;
-}
