622de3b87f2b92dcb8cf0aa55aa25e446ac7ecce
Dipesh Paudel <122140496+paudeldipesh@users.noreply.github.com>
2023-12-29 17:22:46 +0545

    create api

---
+++ app/api/tasks/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import db from "@/utils/db";
+
+export const GET = async (request) => {
+  const tasks = await db.task.findMany();
+
+  return NextResponse.json({
+    data: tasks,
+  });
+};
+
+export const POST = async (request) => {
+  const data = await request.json();
+  const task = await db.task.create({
+    data: {
+      content: data.content,
+    },
+  });
+
+  return Response.json({
+    data: task,
+  });
+};
